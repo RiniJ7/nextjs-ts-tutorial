33af2e77468adc77e591c0fe2e1f60961b4c49eb
rinij <rinimani7@gmail.com>
2024-02-25 16:06:12 -0700

    type defined to resolve typescript error

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 //every page in the app router receives product id as its prop
 //we can destructure it using params. the params object contain route parameter as key value pairs
-const ProductDetails = ({ params }) => {
+const ProductDetails = ({ params,
+ } : { 
+params: { productId: string };
+}) => {
   return (
     <h1>Details about product {params.productId}</h1>
   )
