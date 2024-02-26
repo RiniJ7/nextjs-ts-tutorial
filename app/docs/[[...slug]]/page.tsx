d8f5cb602230b754765e2271e39fd9097c98a78f
rinij <rinimani7@gmail.com>
2024-02-26 12:34:53 -0700

    implemented optional chaining to render the segment at /docs/ level

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -10,13 +10,13 @@ export default function Docs({
 }) {
 
     // it means url contain 2 segment after 'docs'. here it is assumed that the first segment is a feature and the second segment is a concept
-    if(params.slug.length === 2) {
+    if(params.slug?.length === 2) {
         return <h1>
             Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
         </h1>
     }
     // it means url contain 1 segment after 'docs'. here this segment features both features and concepts
-    else if(params.slug.length === 1) {
+    else if(params.slug?.length === 1) {
         return <h1>
             Viewing docs for feature {params.slug[0]}
         </h1>
