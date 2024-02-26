939df1f960aa974333688982cfa9398632eadfc6
rinij <rinimani7@gmail.com>
2024-02-26 11:23:47 -0700

    nested dynamic route implemented using nested segments and params prop in next,js

---
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+//implementation of nested dynamic routes
+
+export default function ReviewDetail({ 
+    params,
+}: {
+    params: {
+        productId: string;
+        reviewId: string;
+    };
+}) {
+  return (
+
+    <h1>
+        Review {params.reviewId} for product {params.productId}</h1>
+  );
+}
