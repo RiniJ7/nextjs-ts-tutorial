522ff8873af2d7ff9e44c3b6df19534653917bcb
rinij <rinimani7@gmail.com>
2024-02-26 17:19:08 -0700

    custom 404 page created

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+
+    //created custom 404 page : you can add custom styling and theme
+    return (
+<div>
+    <h2>Page not found</h2>
+    <p>Could not find requested resource</p>
+</div>
+    )
+}
